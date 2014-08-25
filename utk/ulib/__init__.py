291f0239a2d8429c4ff5e1ad1b20795e34890c13
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-08-25 13:12:22 -0300

    Added MainContext, MainLoop and related methods to ulib

--- utk/ulib/__init__.py
+++ utk/ulib/__init__.py
@@ -1,5 +1,51 @@
 # -*- coding: utf-8 -*-
 
 from signal import (
-    SIGNAL_RUN_FIRST, SIGNAL_RUN_LAST,
+    SIGNAL_RUN_FIRST,
+    SIGNAL_RUN_LAST,
 )
+
+from context import (
+    PRIORITY_HIGH,
+    PRIORITY_DEFAULT,
+    PRIORITY_HIGH_IDLE,
+    PRIORITY_LOW,
+    PRIORITY_DEFAULT_IDLE,
+    main_context_default,
+    MainContext,
+)
+
+from loop import (
+    MainLoop,
+)
+
+# some shortcuts to work with main context
+def idle_add(callback, priority=PRIORITY_DEFAULT_IDLE, context=None):
+    if context is None:
+        context = main_context_default()
+    return context.idle_add(callback, priority)
+
+def idle_remove(handle, context=None):
+    if context is None:
+        context = main_context_default()
+    return context.idle_remove(handle)
+
+def timeout_add_seconds(seconds, callback, context=None):
+    if context is None:
+        context = main_context_default()
+    return context.timeout_add_seconds(seconds, callback)
+
+def timeout_remove(handle, context=None):
+    if context is None:
+        context = main_context_default()
+    return context.timeout_remove(handle)
+
+def io_add_watch(fd, callback, context=None):
+    if context is None:
+        context = main_context_default()
+    return context.io_add_watch(fd, callback)
+
+def io_remove_watch(handle, context=None):
+    if context is None:
+        context = main_context_default()
+    return context.io_remove_watch(handle)
