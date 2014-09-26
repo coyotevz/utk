6be754901e9d18ae7e1b59bb5779b65c448b0cf8
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-09-26 03:46:38 -0300

    Testing Rectangle.diff implementation

--- attic/canv_sim.py
+++ attic/canv_sim.py
@@ -2,13 +2,23 @@
 
 from utk.utils import Rectangle
 
+def diff(r1, r2):
+    i = r1.intersection(r2)
+    res = []
+    if r1.y < i.y:
+        res.append(Rectangle(r1.x, r1.y, r1.width, (r1.height+r1.y)-(i.height+i.y)))
+    if r1.x < i.x:
+        res.append(Rectangle(r1.x, i.y, (r1.width+r1.x)-(i.width+i.x), i.height))
+
+    return res if res else None
+
 class Canvas(object):
 
     def __init__(self, left=0, top=0, cols=1, rows=1):
         self._childs = []
         self._parent = None
         self._area = Rectangle(left, top, cols, rows)
-        self._update= set() # areas to update
+        self._invalids = set() # areas to update
         self.invalidate()
 
     # shortcuts
@@ -25,10 +35,13 @@ class Canvas(object):
     def _unset_dirty(self):
         self._dirty = False
 
-    def invalidate_area(self, area):
+    def _add_invalid(self, area):
+        self._invalids.add(area)
         if self._parent:
             self._parent.invalidate_area(area)
-        self._update.add(area)
+
+    def invalidate_area(self, area):
+        self._add_invalid(area)
         self._set_dirty()
 
     def invalidate(self):
