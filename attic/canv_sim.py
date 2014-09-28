42c747cdd3ddf2ddf56501cef818d50e9e9192d8
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-09-28 03:48:02 -0300

    Refactoring Canvas on canv_sim

--- attic/canv_sim.py
+++ attic/canv_sim.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+
 from utk.utils import Rectangle
+
+class CanvasView(namedtuple("CanvasView", "left top cols rows canvas")):
+    __slots__ = ()
+
+    def content(self):
+        return self.canvas.body_content(self.left, self.top,
+                                        self.cols, self.rows)
+
+    def trim_left(self, trim):
+        return self._replace(left=self.left+trim,
+                             cols=self.cols-trim)
+
+    def trim_top(self, trim):
+        return self._replace(top=self.top+trim,
+                             rows=self.rows-trim)
+
+    def trim_cols(self, cols):
+        return self._replace(cols=cols)
+
+    def trim_rows(self, rows):
+        return self._replace(rows=rows)
+
+
+# Shard structure is composed of number of rows that this shard have, and a
+# list of CanvasView that generates the contents of shard rows
+Shard = namedtuple("Shard", "rows views")
+
 
 class Canvas(object):
 
@@ -9,6 +38,7 @@ class Canvas(object):
         self._parent = None
         self._area = Rectangle(left, top, cols, rows)
         self._invalids = set() # areas to update
+        self._shards = None
         self.invalidate()
 
     # shortcuts
@@ -17,22 +47,24 @@ class Canvas(object):
     cols = property(lambda s: s._area.width)
     rows = property(lambda s: s._area.height)
 
-    def _set_dirty(self):
-        self._dirty = True
-        if self._parent:
-            self._parent._set_dirty()
+    is_dirty = property(lambda s: bool(len(s._invalids)))
 
-    def _unset_dirty(self):
-        self._dirty = False
-
-    def _add_invalid(self, area):
+    def invalidate_area(self, area):
         self._invalids.add(area)
         if self._parent:
             self._parent.invalidate_area(area)
 
-    def invalidate_area(self, area):
-        self._add_invalid(area)
-        self._set_dirty()
-
     def invalidate(self):
         self.invalidate_area(self._area)
+
+    @property
+    def shards(self):
+        if not self.is_dirty:
+            return self._shards
+
+        shard = Shard(self.rows,
+                      [CanvasView(0, 0, self.cols, self.rows, self)])
+
+        self._shards = [shard]
+        self._invalids.clear()
+        return self._shards
